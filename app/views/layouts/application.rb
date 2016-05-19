ca4e1c636bb8a3c3a3c1175dc3a98fc238052f68
Adam Becker <ad@mbecker.cc>
2016-05-19 12:09:11 -0700

    wire up GA

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -48,7 +48,17 @@ class Views::Layouts::Application < Views::Base
         render_widgets(:modals)
         rawtext '<!--[if lt IE 9]><script src="//d2yxgjkkbvnhdt.cloudfront.net/dist/polyfills.js"></script><![endif]-->'
 
-        # @todo install Google Analytics here
+        rawtext %{
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-39205841-29', 'auto');
+            ga('send', 'pageview');
+          </script>
+        }.squish
       }
     }
   end
